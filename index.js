551f60802e2d1a74f110252c31ac460c1d943e1e
yuffiy <yfhj1990@hotmail.com>
2016-07-20 19:16:02 +0800

    update

---
+++ index.js
@@ -0,0 +1,22 @@
+import $ from 'jquery'
+
+$('.addresspicker').on('click', '.field', evt => {
+  $(evt.delegateTarget)
+    .find('.select-options-container')
+    .toggleClass('active')
+}).on('click', '.select-option', evt => {
+  $(evt.delegateTarget)
+    .find('.select-options-container')
+    .removeClass('active')
+})
+
+$('.datepicker').on('click', '.field', evt => {
+  $(evt.delegateTarget)
+    .find('.select-options-container')
+    .addClass('active')
+}).on('click', '.datepicker-header, .datepicker-cell, .button', evt => {
+  $(evt.delegateTarget)
+    .find('.select-options-container')
+    .removeClass('active')
+})
+ 
